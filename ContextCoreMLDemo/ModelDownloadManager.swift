c426b9384cee0d88257aa5a1332df5b19c5f8e04
Felix Krause <felix@krausefx.com>
2024-05-16 18:36:50 +0200

    Update code samples

---
+++ ContextCoreMLDemo/ModelDownloadManager.swift
@@ -0,0 +1,91 @@
+import Foundation
+import CoreML
+
+class ModelDownloadManager {
+    private let fileManager: FileManager
+    private let modelsFolder: URL
+    private let modelUpdateCheckURL = "https://krausefx.github.io/CoreMLDemo/latest_model_details.json"
+
+    init(fileManager: FileManager = .default) {
+        self.fileManager = fileManager
+        if let folder = fileManager.urls(for: .applicationSupportDirectory, in: .userDomainMask).first?.appendingPathComponent("context_sdk_models") {
+            self.modelsFolder = folder
+            try? fileManager.createDirectory(at: folder, withIntermediateDirectories: true)
+        } else {
+            fatalError("Unable to find or create models folder.") // Handle this more gracefully
+        }
+    }
+    
+    internal func latestModel() -> MyFirstCustomModel? {
+        let fileManagerContents = (try? fileManager.contentsOfDirectory(at: modelsFolder, includingPropertiesForKeys: nil)) ?? []
+        
+        // Try to find the latest model file based on modification date
+        if let latestFileURL = fileManagerContents.sorted(by: { $0.lastPathComponent > $1.lastPathComponent }).first,
+            let otaModel = try? MyFirstCustomModel(contentsOf: latestFileURL) {
+            return otaModel
+        } else if let bundledModel = try? MyFirstCustomModel(configuration: MLModelConfiguration()) {
+            // Fallback to the bundled model if no downloaded model exists
+            return bundledModel
+        }
+        return nil
+    }
+    
+    internal func checkForModelUpdates() async throws {
+        guard let url = URL(string: modelUpdateCheckURL) else {
+            throw URLError(.badURL)
+        }
+        let (data, _) = try await URLSession.shared.data(from: url)
+        guard let jsonObject = try JSONSerialization.jsonObject(with: data) as? [String: Any],
+              let modelDownloadURLString = jsonObject["url"] as? String,
+              let modelDownloadURL = URL(string: modelDownloadURLString) else {
+            throw URLError(.cannotParseResponse)
+        }
+        
+        try await downloadIfNeeded(from: modelDownloadURL)
+    }
+    
+    private func downloadIfNeeded(from url: URL) async throws {
+        let lastPathComponent = url.lastPathComponent
+        
+        // Check if the model file already exists (for this sample project we use the unique file name as identifier)
+        if let localFiles = try? fileManager.contentsOfDirectory(at: modelsFolder, includingPropertiesForKeys: nil),
+           localFiles.contains(where: { $0.lastPathComponent == lastPathComponent }) {
+            // File exists, you could add a version check here if versions are part of the file name or metadata
+            print("Model already exists locally. No need to download.")
+        } else {
+            // File does not exist, download it
+            let downloadedURL = try await downloadCoreMLFile(from: url)
+            _ = try compileCoreMLFile(at: downloadedURL)
+            print("Model downloaded and compiled successfully.")
+        }
+    }
+    
+    // It's important to immediately move the downloaded CoreML file into a permanent location
+    private func downloadCoreMLFile(from url: URL) async throws -> URL {
+        // Download file using URLSession's native async method
+        let (tempLocalURL, _) = try await URLSession.shared.download(for: URLRequest(url: url))
+        
+        // Destination URL in the models folder
+        let destinationURL = modelsFolder.appendingPathComponent(tempLocalURL.lastPathComponent)
+        
+        // Move the downloaded file to the models folder
+        try fileManager.moveItem(at: tempLocalURL, to: destinationURL)
+        
+        return destinationURL
+    }
+    
+    private func compileCoreMLFile(at localFilePath: URL) throws -> URL {
+        let compiledModelURL = try MLModel.compileModel(at: localFilePath)
+        let destinationCompiledURL = modelsFolder.appendingPathComponent(compiledModelURL.lastPathComponent)
+        try fileManager.moveItem(at: compiledModelURL, to: destinationCompiledURL)
+        try fileManager.removeItem(at: localFilePath)
+        return destinationCompiledURL
+    }
+    
+    private func deleteAllOutdatedModels(keeping recentModelFileName: String) throws {
+        let urlContent = try fileManager.contentsOfDirectory(at: modelsFolder, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+        for fileURL in urlContent where fileURL.lastPathComponent != recentModelFileName {
+            try fileManager.removeItem(at: fileURL)
+        }
+    }
+}
--- ContextCoreMLDemo/ViewController.swift
+++ ContextCoreMLDemo/ViewController.swift
@@ -2,10 +2,13 @@ import UIKit
 import CoreML
 
 class ViewController: UIViewController {
+    let modelDownloadManager = ModelDownloadManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    }
+    
+    @IBAction func runLatestLocalModel() {
         let batteryLevel = UIDevice.current.batteryLevel
         let batteryCharging = UIDevice.current.batteryState == .charging || UIDevice.current.batteryState == .full
         do {
@@ -13,14 +16,34 @@ class ViewController: UIViewController {
                Double(batteryLevel),
                Double(batteryCharging ? 1.0 : 0.0)
             ])
-            let result = try MyFirstCustomModel(configuration: MLModelConfiguration()).prediction(input: modelInput)
-            let classProbabilities = result.featureValue(for: "classProbability")?.dictionaryValue
-            let upsellProbability = classProbabilities?["Purchased"]?.doubleValue ?? -1
-
-            print("Chances of Upsell: \(upsellProbability)")
+            if let result = try modelDownloadManager.latestModel()?.prediction(input: modelInput) {
+                let classProbabilities = result.featureValue(for: "classProbability")?.dictionaryValue
+                let upsellProbability = classProbabilities?["Purchased"]?.doubleValue ?? -1
+                
+                showAlertDialog(message:("Chances of Upsell: \(upsellProbability)"))
+            } else {
+                showAlertDialog(message:("Could not run CoreML model"))
+            }
         } catch {
-            print("Error running CoreML file: \(error)")
+            showAlertDialog(message:("Error running CoreML file: \(error)"))
         }
     }
+    
+    @IBAction func downloadLatestModel() {
+        Task {
+            do {
+                try await modelDownloadManager.checkForModelUpdates()
+                showAlertDialog(message:("Model update completed successfully."))
+            } catch {
+                // Handle possible errors here
+                showAlertDialog(message:("Failed to update model: \(error.localizedDescription)"))
+            }
+        }
+    }
+    
+    internal func showAlertDialog(message: String) {
+        let alert = UIAlertController(title: "Alert", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
 }
-
