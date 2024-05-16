7a1384fb1302ebd0305d24dbc12537ea201f376e
Felix Krause <felix@krausefx.com>
2024-05-16 09:41:29 +0200

    Initial commit

---
+++ ContextCoreMLDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContextCoreMLDemo
+//
+//  Created by Felix Krause on 16.05.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
