293c3a26e4b1fd93fbd395a71f0672e03a232f7c
Felix Krause <felix@krausefx.com>
2024-05-16 12:39:54 +0200

    Initial code sample

--- ContextCoreMLDemo/ViewController.swift
+++ ContextCoreMLDemo/ViewController.swift
@@ -1,19 +1,26 @@
-//
-//  ViewController.swift
-//  ContextCoreMLDemo
-//
-//  Created by Felix Krause on 16.05.24.
-//
-
 import UIKit
+import CoreML
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let batteryLevel = UIDevice.current.batteryLevel
+        let batteryCharging = UIDevice.current.batteryState == .charging || UIDevice.current.batteryState == .full
+        do {
+            let modelInput = MyFirstCustomModelInput(input: [
+               Double(batteryLevel),
+               Double(batteryCharging ? 1.0 : 0.0)
+            ])
+            let result = try MyFirstCustomModel(configuration: MLModelConfiguration()).prediction(input: modelInput)
+            let classProbabilities = result.featureValue(for: "classProbability")?.dictionaryValue
+            let upsellProbability = classProbabilities?["Purchased"]?.doubleValue ?? -1
+
+            print("Chances of Upsell: \(upsellProbability)")
+        } catch {
+            print("Error running CoreML file: \(error)")
+        }
     }
-
-
 }
 
